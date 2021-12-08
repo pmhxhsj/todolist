f1814a35d6b4e9634d72ddf8c01ca4971d38401f
pmhxhsj <minho7952@naver.com>
2021-12-09 03:05:24 +0900

    feat: 수정 및 삭제 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,65 @@
-document.querySelector('#input-form').addEventListener('submit', (e) => {
+import { ALERT_LIST } from './inputException.js';
+
+const $ = (selector) => document.querySelector(selector);
+
+$('#input-form').addEventListener('submit', (e) => {
   e.preventDefault();
 });
 
 const showToDoList = () => {
-  const $todo = document.querySelector('#todo-input').value;
-  const toDoList = document.createElement('div');
-  const toDoListContents = document.createTextNode($todo);
+  const eachToDoList = $('#todo-input').value;
+  const toDoListTemplate = (eachToDoList) => `
+    <li class="to-do-list-item" style="list-style-type: decimal;">
+      <span class="list-name">${eachToDoList}</span>
+      <button
+        type="button"
+        class="list-edit-button"
+        >
+          edit
+        </button>
+        <button
+          type="button"
+          class="list-delete-button"
+          style="color:red"
+        >
+          delete
+        </button>
+    </li>`;
+  $('.to-do-list').insertAdjacentHTML(
+    'beforeend',
+    toDoListTemplate(eachToDoList),
+  );
 
-  toDoList.appendChild(toDoListContents);
-  document.querySelector('#App').appendChild(toDoList);
-
-  document.querySelector('#todo-input').value = '';
+  $('#todo-input').value = '';
 };
 
 function App() {
-  document.querySelector('#input-button').addEventListener('click', () => {
+  $('#input-button').addEventListener('click', () => {
+    if ($('#todo-input').value === '') {
+      alert(ALERT_LIST.BLANK);
+      return;
+    }
     showToDoList();
+  });
+
+  $('.to-do-list').addEventListener('click', (e) => {
+    if (e.target.classList.contains('list-edit-button')) {
+      const listName = e.target
+        .closest('li')
+        .querySelector('.list-name').innerText;
+      const updateListName = prompt(
+        'Please enter the to do list to be edited',
+        listName
+      );
+      e.target.closest('li').querySelector('.list-name').innerText =
+        updateListName;
+    }
+
+    if (e.target.classList.contains('list-delete-button')) {
+      if (confirm('Are you sure you want to delete?')) {
+        e.target.closest('li').remove();
+      }
+    }
   });
 }
 
