baf03ac32a84eb58a3ce7c96c368a274bfdaf0fb
pmhxhsj <minho7952@naver.com>
2021-12-08 23:32:41 +0900

    feat: todolist 기본적 기능 구현 - 입력 시 html 핸들링 및 list 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,22 @@
 document.querySelector('#input-form').addEventListener('submit', (e) => {
   e.preventDefault();
 });
+
+const showToDoList = () => {
+  const $todo = document.querySelector('#todo-input').value;
+  const toDoList = document.createElement('div');
+  const toDoListContents = document.createTextNode($todo);
+
+  toDoList.appendChild(toDoListContents);
+  document.querySelector('#App').appendChild(toDoList);
+
+  document.querySelector('#todo-input').value = '';
+};
+
+function App() {
+  document.querySelector('#input-button').addEventListener('click', () => {
+    showToDoList();
+  });
+}
+
+App();
